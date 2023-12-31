e39b91971c30bc488dd9879bc10e4951ee950b35
Ahammed Irfan Ali <69192055+irfanpa005@users.noreply.github.com>
2023-12-31 17:32:04 +0530

    firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,7 +4,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  //firebase config goes here
+  apiKey: "AIzaSyDXAtkDwHyXwYl0DJlkYNwmgZuArsk6_9Y",
+  authDomain: "olx-clone-react-54007.firebaseapp.com",
+  projectId: "olx-clone-react-54007",
+  storageBucket: "olx-clone-react-54007.appspot.com",
+  messagingSenderId: "939418810283",
+  appId: "1:939418810283:web:894d74c93d7dcc043784ee",
+  measurementId: "G-2CPWNSK88E"
 };
 
 // Initialize Firebase
